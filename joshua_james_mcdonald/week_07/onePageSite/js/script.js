02274957bd8ba15d89bbb21e3338c41130284540
Joshua McDonald <mcdojj12@gmail.com>
2020-06-30 15:49:29 +1000

    finished ISS tracker

---
+++ joshua_james_mcdonald/week_07/onePageSite/js/script.js
@@ -0,0 +1,13 @@
+const fetchLocation = function(){
+    $.ajax('http://api.open-notify.org/iss-now.json').done(function(data){
+        $('#position-text').text(JSON.stringify(data.iss_position));
+        let top = 100-(data.iss_position.latitude*100/180+50);
+        let left = (data.iss_position.longitude*100/360+50);
+        $('#converted-text').text(`left: ${left} top: ${top} `);
+        $('#iss-image').css({'top': `${top}%`, 'left': `${left}%`});
+        
+    })
+}
+// 0,0 at 80, -180
+fetchLocation()
+setInterval(fetchLocation, 5000); // want to put this is a while 1 loop, with 1 second delays
