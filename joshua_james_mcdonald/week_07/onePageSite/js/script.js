988e3e6e182bbcc3bd2dd0fc6275e05b7f0fee70
Joshua McDonald <mcdojj12@gmail.com>
2020-06-30 16:21:42 +1000

    added breadcrumbs for the ISS and adjusted z-index

--- joshua_james_mcdonald/week_07/onePageSite/js/script.js
+++ joshua_james_mcdonald/week_07/onePageSite/js/script.js
@@ -4,8 +4,14 @@ const fetchLocation = function(){
         let top = 100-(data.iss_position.latitude*100/180+50);
         let left = (data.iss_position.longitude*100/360+50);
         $('#converted-text').text(`left: ${left} top: ${top} `);
-        $('#iss-image').css({'top': `${top}%`, 'left': `${left}%`});
-        
+        $('.images').append(`<img 
+        class="breadcrumb" 
+        src="img/red-dot.png" 
+        style=
+        "left: ${left}%;
+        top: ${top}%"
+        > </img>`)
+        $('#iss-image').css({'top': `${top-4}%`, 'left': `${left-4}%`});
     })
 }
 // 0,0 at 80, -180
