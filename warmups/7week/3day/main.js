5faa3f10ba5f295cd9141bfc551f9c5b8395b0dc
aleksanderbrymora <aleksander.australia@gmail.com>
2020-07-01 10:32:08 +1000

    w7d3s

--- warmups/7week/3day/main.js
+++ warmups/7week/3day/main.js
@@ -3,34 +3,59 @@ const sieve = {
 	list: [],
 	calc: function (num) {
 		this.list = Array(num).fill(true);
+
 		for (let i = 2; i <= num; i++) {
 			for (let j = i * i; j <= num; j++) {
-				if (this.list[j] === true && j % i === 0) {
+				if (this.list[j] && j % i === 0) {
 					this.list[j] = false;
 				}
 			}
 		}
-		for (let i = 2; i < this.list.length; i++) {
+
+		for (let i = 2; i <= this.list.length; i++) {
 			if (this.list[i]) {
 				this.primes.push(i);
 			}
 		}
+
 		return this.primes;
 	},
 };
 
-console.log(sieve.calc(20));
+const sieve2 = function (num) {
+	const primes = [];
+	const list = Array(num).fill(true);
 
-const sieve2 = function (limit) {
+	for (let i = 2; i <= Math.sqrt(num); i++) {
+		for (let j = i * i; j <= num; j++) {
+			if (list[j] && j % i === 0) {
+				list[j] = false;
+			}
+		}
+	}
+
+	for (let i = 2; i <= list.length; i++) {
+		if (list[i]) {
+			primes.push(i);
+		}
+	}
+
+	return primes;
+};
+
+const sieve3 = function (limit) {
 	let nums = [...Array(limit + 1).keys()].slice(2);
 	let primes = [];
 
 	while (nums.length) {
-		let n = nums.shift(); //take out first element
-		primes.push(n); //push to list of primes
-		nums = nums.filter((m) => m % n); //remove multiples from remaining nums
+		let n = nums.shift();
+		primes.push(n);
+		nums = nums.filter(function (el) {
+			return el % n; // checking for not divisible, truthy falsey
+		});
 	}
+
 	return primes;
 };
 
-console.log(sieve2(20));
+console.log(sieve3(20));
