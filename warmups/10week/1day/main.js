5fa11bd48342b87ba9d3991909bcc7c917293f7d
aleksanderbrymora <aleksander.australia@gmail.com>
2020-07-20 09:43:27 +1000

    w10d1 solutions

---
+++ warmups/10week/1day/main.js
@@ -0,0 +1,42 @@
+const reverse = (str, out = '', index = str.length - 1) => {
+	if (str.length === out.length) return out;
+	return reverse(str, out + str[index], index - 1);
+};
+
+const printOdds = (num = 1) => {
+	if (num > 99) return;
+	if (num % 2 !== 0) console.log(num);
+	printOdds(num + 1);
+};
+
+const printOddsNicely = (num = 1, arr = []) => {
+	if (num > 99) return arr.join(' ');
+	return num % 2 !== 0
+		? printOddsNicely(num + 1, [...arr, num])
+		: printOddsNicely(num + 1, arr);
+};
+
+const largest = (arr, large = -Infinity, index = 0) => {
+	if (arr.length - 1 === index) return large;
+	return arr[index] > large
+		? largest(arr, arr[index], index + 1)
+		: largest(arr, large, index + 1);
+};
+
+const fib = (num, prev = 1, now = 1, index = 0) =>
+	num === index ? now : fib(num, now, now + prev, index + 1);
+
+console.log('====Reverse===================');
+console.log(reverse('pls reverse'));
+
+console.log('====Odds======================');
+printOdds();
+
+console.log('====Nice odds=================');
+console.log(printOddsNicely());
+
+console.log('====Largest===================');
+console.log(largest([1, 2, 3, 4, 5, 5]));
+
+console.log('====Fibonacci=================');
+console.log(fib(5));
