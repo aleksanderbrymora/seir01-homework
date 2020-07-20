5303d8cecadf8f0e06ad9b0679bb49f42841a2ba
aleksanderbrymora <aleksander.australia@gmail.com>
2020-07-20 10:34:37 +1000

    solutions

--- warmups/10week/1day/main.js
+++ warmups/10week/1day/main.js
@@ -1,6 +1,6 @@
-const reverse = (str, out = '', index = str.length - 1) => {
+const reverseMe = (str, out = '', index = str.length - 1) => {
 	if (str.length === out.length) return out;
-	return reverse(str, out + str[index], index - 1);
+	return reverseMe(str, out + str[index], index - 1);
 };
 
 const printOdds = (num = 1) => {
@@ -17,26 +17,34 @@ const printOddsNicely = (num = 1, arr = []) => {
 };
 
 const largest = (arr, large = -Infinity, index = 0) => {
-	if (arr.length - 1 === index) return large;
+	if (arr.length === index) return large;
 	return arr[index] > large
 		? largest(arr, arr[index], index + 1)
 		: largest(arr, large, index + 1);
 };
 
-const fib = (num, prev = 1, now = 1, index = 0) =>
-	num === index ? now : fib(num, now, now + prev, index + 1);
+// const fib = (num, prev = 1, now = 1, index = 0) =>
+// 	num === index ? now : fib(num, now, now + prev, index + 1);
+
+const fib = (num, prev = 1, now = 1, index = 2) => {
+	if (num === index || num === 0 || num == 1) {
+		return now;
+	} else {
+		return fib(num, now, now + prev, index + 1);
+	}
+};
 
 console.log('====Reverse===================');
-console.log(reverse('pls reverse'));
+console.log(reverseMe('pls reverse'));
 
-console.log('====Odds======================');
-printOdds();
+// console.log('====Odds======================');
+// printOdds();
 
 console.log('====Nice odds=================');
 console.log(printOddsNicely());
 
 console.log('====Largest===================');
-console.log(largest([1, 2, 3, 4, 5, 5]));
+console.log(largest([41, 1, 2, 3, 4, 5, 55]));
 
 console.log('====Fibonacci=================');
-console.log(fib(5));
+console.log(fib(0));
