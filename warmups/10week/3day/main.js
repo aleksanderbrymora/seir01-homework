0b8032647bb9f216fdd233e05d028884a4ecc939
aleksanderbrymora <aleksander.australia@gmail.com>
2020-07-22 10:51:49 +1000

    solutions

--- warmups/10week/3day/main.js
+++ warmups/10week/3day/main.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const table = () => {
 	// const out = [];
-	// for (let i = 0; i < 13; i++) {
+	// for (let i = 0; i <= 12; i++) {
 	// 	const row = [];
 	// 	for (let j = 0; j < 13; j++) {
 	// 		row.push(i * j);
@@ -11,24 +11,30 @@ const table = () => {
 	// }
 	// return out;
 	const genNums = () => [...Array(13).keys()];
-	return genNums().map((row) => genNums().map((num) => num * row));
+	return genNums().map((row) => genNums().map((num) => row * num));
 };
+
+// console.table(table());
 
 const sumtxt = () => {
 	nums = fs.readFileSync('./numbers.txt', 'utf-8');
 	return nums.split('\n').reduce((acc, curr) => acc + parseInt(curr), 0);
 };
 
+console.log(sumtxt());
+
 const rgbtohex = (rgb) => {
-	const toHex = (num) => (num >= 10 ? String.fromCharCode(num + 55) : val);
-	return rgb.reduce(
-		(acc, curr) =>
-			acc + toHex(Math.floor(curr / 16) + toHex(toHex(curr % 16), '#')),
-	);
+	const toHex = (val) => (val >= 10 ? String.fromCharCode(55 + val) : val);
+	let out = '#';
+	rgb.forEach((color) => {
+		out += toHex(Math.floor(color / 16));
+		out += toHex(color % 16);
+	});
+	return out;
+	// return rgb.reduce(
+	// 	(acc, color) => acc + toHex(Math.floor(color / 16) + toHex(color % 16)),
+	// 	'#',
+	// );
 };
 
-console.table(table());
-console.log('================');
-console.log(sumtxt());
-console.log('================');
-console.log(rgbtohex([13, 242, 144]));
+console.log(rgbtohex([113, 23, 255]));
