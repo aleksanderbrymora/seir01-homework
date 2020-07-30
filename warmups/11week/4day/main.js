52f423945ab363f1ae4fc150af509e74b54b8a63
aleksanderbrymora <aleksander.australia@gmail.com>
2020-07-30 10:27:14 +1000

    more solutions

--- warmups/11week/4day/main.js
+++ warmups/11week/4day/main.js
@@ -1,20 +1,47 @@
-// const dashatize = (str) => Number.isNaN(str) ? str : str.toString().split('').filter((d) => d !== '-').reduce((acc,curr,i,all) => (acc += parseInt(curr) % 2 !== 0 && i !== 0 && i !== all - 1 ? `-${curr}` : curr)).replace(/--/g,'-');
+// const dashatize = (str) => {
+// 	if (Number.isNaN(str)) return str;
+// 	str = Math.abs(str);
+// 	str = str.toString();
+// 	const digits = str.split('');
+// 	// digits.filter(d => d !== '-') // similar to str = Math.abs(str)
+// 	let out = '';
+// 	for (let i = 0; i < digits.length; i++) {
+// 		if (parseInt(digits[i]) % 2 !== 0) {
+// 			out += '-' + digits[i] + '-';
+// 		} else {
+// 			out += digits[i];
+// 		}
+// 	}
+// 	out = out.replace(/--/g, '-');
+// 	if (out[out.length - 1] === '-') out = out.slice(0, -1);
+// 	if (out[0] === '-') out = out.slice(1);
+// 	return out;
+// };
 
-const dashatize = (str) =>
-	Number.isNaN(str)
-		? str
-		: str
-				.toString()
-				.split('')
-				.filter((d) => d !== '-')
-				.reduce(
-					(acc, curr, i, all) =>
-						(acc +=
-							parseInt(curr) % 2 !== 0 && i !== 0 && i !== all - 1
-								? `-${curr}`
-								: curr),
-				)
-				.replace(/--/g, '-');
+// const dashatize = (str) =>
+// 	Number.isNaN(str)
+// 		? str
+// 		: str
+// 				.toString()
+// 				.split('')
+// 				.filter((d) => d !== '-')
+// 				.reduce(
+// 					(acc, curr, i, all) =>
+// 						(acc +=
+// 							parseInt(curr) % 2 !== 0 && i !== 0 && i !== all - 1
+// 								? `-${curr}`
+// 								: curr),
+// 				)
+// 				.replace(/--/g, '-');
+
+function dashatize(num) {
+	return num
+		.toString()
+		.replace(/([13579])/g, '-$1-') // dashes around odd digits
+		.replace(/\-+/g, '-') // replace multiple dashes by one dash
+		.replace(/^\-/, '') // remove starting dash
+		.replace(/\-$/, ''); // remove ending dash
+}
 
 console.log(dashatize(274));
 console.log(dashatize(5311));
